c9082ed3acffe12f582bdaf605a0ac729085f758
Quedale <sebaslau@gmail.com>
2023-09-27 21:34:45 -0400

    portable_io.h - fixed gcc support regression

--- src/portable_io.h
+++ src/portable_io.h
@@ -8,6 +8,9 @@
 #   define P_UNLOCK_FILE(x) UnlockFileEx(x, 0, MAXDWORD, 0, &overlapvar);
 #   define strtok_r strtok_s
 #else
+#   include <stdlib.h>
+#   define sprintf_s(x,y,z,...) sprintf(x,z,##__VA_ARGS__)
+#   define strcpy_s(x,y,z) strcpy(x,z)
 #   define P_LOCK_FILE(x) flockfile(x)
 #   define P_UNLOCK_FILE(x) funlockfile(x)
 #endif
